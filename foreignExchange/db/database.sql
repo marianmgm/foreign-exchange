c831473b7bdbf6872d82a7d949f28221db92693c
marianmgm <138883077+marianmgm@users.noreply.github.com>
2024-04-12 19:41:34 +0300

    update database with transaction table

--- foreignExchange/db/database.sql
+++ foreignExchange/db/database.sql
@@ -1,4 +1,4 @@
-create table exchange
+create table exchange_rate
 (
     exchange_rate_id int auto_increment
         primary key,
@@ -6,4 +6,15 @@ create table exchange
     target_currency  varchar(3)     not null,
     rate             decimal(10, 6) not null,
     time_stamp       datetime       not null
+);
+
+create table transaction
+(
+    transaction_id  int auto_increment
+        primary key,
+    source_currency varchar(3)     not null,
+    source_amount   decimal(10, 2) not null,
+    target_currency varchar(3)     not null,
+    target_amount   decimal(10, 2) not null,
+    time_stamp      datetime       not null
 );
